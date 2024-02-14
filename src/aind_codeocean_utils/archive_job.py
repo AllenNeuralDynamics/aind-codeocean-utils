e8377cc026feab69574de781477f2c253c11bce0
dyf <dyf@fake.email>
2024-02-13 18:23:18 -0800

    filtering is working

--- src/aind_codeocean_utils/archive_job.py
+++ src/aind_codeocean_utils/archive_job.py
@@ -1,5 +1,5 @@
 from aind_codeocean_api.codeocean import CodeOceanClient
-from datetime import timedelta
+from datetime import datetime
 
 class ArchiveDeleteJob:
     """
@@ -10,14 +10,27 @@ class ArchiveDeleteJob:
     def __init__(self, co_client: CodeOceanClient):
         self.client = co_client
 
-    def find_archived_data_assets(self, minimum_age: timedelta):
-        assets = self.client.search_all_data_assets(archived=True).json()
+    def delete_data_assets(self, keep_after: datetime, dry_run: bool = True):
+        assets = self.client.search_all_data_assets(archived=True).json()['results']
         
+
+        for asset in assets:
+            created = datetime.fromtimestamp(asset['created'])
+            last_used = datetime.fromtimestamp(asset['last_used']) if asset['last_used'] != 0 else None
+
+            old = created < keep_after
+            not_used_recently = not last_used or last_used < keep_after
+            if old and not_used_recently:
+                yield asset
+
 
 if __name__ == "__main__":
     from aind_codeocean_api.credentials import CodeOceanCredentials
+    from datetime import timedelta
     creds = CodeOceanCredentials()
     client = CodeOceanClient.from_credentials(creds)
     j = ArchiveDeleteJob(client)
 
-    jobs = j.find_archived_data_assets(minimum_age=timedelta(days=30))
+    assets_to_delete = j.find_archived_data_assets(keep_after=datetime.now() - timedelta(days=30))
+    for asset in assets_to_archive:
+        print(asset)
