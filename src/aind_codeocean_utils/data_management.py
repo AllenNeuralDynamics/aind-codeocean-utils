47b4b961363f3e5d42d35a80a3de90928d5a67b0
dyf <dyf@fake.email>
2024-02-14 21:37:26 -0800

    class

--- src/aind_codeocean_utils/data_management.py
+++ src/aind_codeocean_utils/data_management.py
@@ -10,77 +10,87 @@ from botocore.errorfactory import ClientError
 logger = logging.getLogger(__name__)
 
 
-def find_archived_data_assets_to_delete(client, keep_after: datetime):
-    """find archived data assets that are safe to delete"""
+class DataManager:
+    def __init__(self, client):
+        self.client = client
+        self._s3 = None
 
-    assets = client.search_all_data_assets(archived=True).json()["results"]
+    @property
+    def s3(self):
+        if self._s3 is None:
+            self._s3 = boto3.client("s3")
+        return self._s3
 
-    assets_to_delete = []
+    def find_archived_data_assets_to_delete(self, keep_after: datetime):
+        """find archived data assets that are safe to delete"""
 
-    for asset in assets:
-        created = datetime.fromtimestamp(asset["created"])
-        last_used = (
-            datetime.fromtimestamp(asset["last_used"])
-            if asset["last_used"] != 0
-            else None
-        )
+        assets = self.client.search_all_data_assets(archived=True).json()[
+            "results"
+        ]
 
-        old = created < keep_after
-        not_used_recently = not last_used or last_used < keep_after
+        assets_to_delete = []
 
-        if old and not_used_recently:
-            assets_to_delete.append(asset)
+        for asset in assets:
+            created = datetime.fromtimestamp(asset["created"])
+            last_used = (
+                datetime.fromtimestamp(asset["last_used"])
+                if asset["last_used"] != 0
+                else None
+            )
 
-    external_size = 0
-    internal_size = 0
-    for asset in assets_to_delete:
-        size = asset.get("size", 0)
-        is_external = "sourceBucket" in asset
-        if is_external:
-            external_size += size
-        else:
-            internal_size += size
-        logger.info(f"{asset['name']} {asset['type']}")
+            old = created < keep_after
+            not_used_recently = not last_used or last_used < keep_after
 
-    logger.info(f"{len(assets)} archived data assets can be deleted")
-    logger.info(f"{internal_size / 1e9} GBs internal")
-    logger.info(f"{external_size / 1e9} GBs external")
+            if old and not_used_recently:
+                assets_to_delete.append(asset)
 
-    return assets_to_delete
+        external_size = 0
+        internal_size = 0
+        for asset in assets_to_delete:
+            size = asset.get("size", 0)
+            is_external = "sourceBucket" in asset
+            if is_external:
+                external_size += size
+            else:
+                internal_size += size
+            logger.info(f"{asset['name']} {asset['type']}")
 
+        logger.info(f"{len(assets)} archived data assets can be deleted")
+        logger.info(f"{internal_size / 1e9} GBs internal")
+        logger.info(f"{external_size / 1e9} GBs external")
 
-def find_external_assets(client):
-    """find all external data assets"""
+        return assets_to_delete
 
-    assets = client.search_all_data_assets(type="dataset").json()["results"]
-    for asset in assets:
-        bucket = asset.get("sourceBucket", {}).get("bucket", None)
-        if bucket:
-            yield asset
+    def find_external_assets(self):
+        """find all external data assets"""
 
-
-def find_nonexistant_external_data_assets(client):
-    """find external data assets that do not exist"""
-
-    s3 = boto3.client("s3")
-
-    for asset in find_external_assets(client):
-        sb = asset["sourceBucket"]
-
-        try:
-            exists = bucket_folder_exists(s3, sb["bucket"], sb["prefix"])
-            logger.info(f"{sb['bucket']} {sb['prefix']} exists? {exists}")
-            if not exists:
+        assets = self.client.search_all_data_assets(type="dataset").json()[
+            "results"
+        ]
+        for asset in assets:
+            bucket = asset.get("sourceBucket", {}).get("bucket", None)
+            if bucket:
                 yield asset
-        except botocore.exceptions.ClientError as e:
-            logger.warning(e)
 
+    def find_nonexistent_external_data_assets(self):
+        """find external data assets that do not exist"""
 
-def bucket_folder_exists(s3, bucket, path) -> bool:
-    """Check if folder exists. Folder could be empty."""
+        for asset in find_external_assets(client):
+            sb = asset["sourceBucket"]
 
-    path = path.rstrip("/")
-    resp = s3.list_objects(
-        Bucket=bucket, Prefix=path, Delimiter="/", MaxKeys=1
-    )
-    return "CommonPrefixes" in resp
+            try:
+                exists = bucket_folder_exists(s3, sb["bucket"], sb["prefix"])
+                logger.info(f"{sb['bucket']} {sb['prefix']} exists? {exists}")
+                if not exists:
+                    yield asset
+            except botocore.exceptions.ClientError as e:
+                logger.warning(e)
+
+    def bucket_folder_exists(self, bucket, path) -> bool:
+        """Check if folder exists. Folder could be empty."""
+
+        path = path.rstrip("/")
+        resp = self.s3.list_objects(
+            Bucket=bucket, Prefix=path, Delimiter="/", MaxKeys=1
+        )
+        return "CommonPrefixes" in resp
